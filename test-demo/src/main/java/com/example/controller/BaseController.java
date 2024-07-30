88811e4273a5e9c795abb59da93a31dfad97294e
marlon <471400281@qq.com>
2024-07-30 21:31:45 +0800

    add controller

---
+++ test-demo/src/main/java/com/example/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+/**
+ * @author Marlon
+ * @date 2024/07/30 21:31
+ **/
+public class BaseController {
+    public static void main(String[] args) {
+        System.out.println("Base controller");
+    }
+}
