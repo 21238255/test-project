90e948850f9836cb6110dad0d93908c79281d003
marlon <471400281@qq.com>
2024-07-30 21:28:39 +0800

    test

---
+++ test-demo/src/main/java/com/example/Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author Marlon
+ * @date 2024/07/30 21:25
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!" + " This is a Java program!" + " Marlon is learning Java");
+    }
+
+}
