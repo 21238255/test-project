934f50a6b148809b90a1aa5c3832c1b0419b8d0f
marlon <471400281@qq.com>
2024-08-03 16:56:54 +0800

    springboot framework

---
+++ framework-springboot/springboot/src/main/java/com/example/MarlonSpringApplicationBuilder.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.application.Application;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+
+/**
+ * @author Marlon
+ * @date 2024/08/03 15:04
+ **/
+public class MarlonSpringApplicationBuilder {
+        // 自定义Spring Boot应用构建器
+    public MarlonSpringApplicationBuilder() {
+    }
+    // 构建 Spring Boot应用
+    public MarlonSpringApplicationBuilder build() {
+        return new MarlonSpringApplicationBuilder();
+    }
+    // 向 Spring Boot应用添加 Bean
+
+    // 向 Spring Boot应用添加 Bean 工厂
+
+    // 获取 Spring Boot 应用
+
+
+
+}
---
+++ framework-springboot/user/src/main/java/com/example/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @author Marlon
+ * @date 2024/08/03 14:53
+ **/
+@MarlonSpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        MarlonSpringApplication.run(MyApplication.class);
+    }
+}
